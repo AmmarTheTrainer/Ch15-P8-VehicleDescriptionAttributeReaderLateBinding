4c1c87b275779ae3de6ed70992f78d2ea2eacfe6
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-11-17 18:59:37 +0500

    final update from KQ

--- Ch15-P8-VehicleDesAttributeRdrLateBinding/Program.cs
+++ Ch15-P8-VehicleDesAttributeRdrLateBinding/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,39 @@ namespace Ch15_P8_VehicleDesAttributeRdrLateBinding
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Value of VehicleDescriptionAttribute *****\n");
+            ReflectAttributesUsingLateBinding();
+            Console.ReadLine();
+        }
 
+        private static void ReflectAttributesUsingLateBinding()
+        {
+            try
+            {
+                // Load the local copy of AttributedCarLibrary.
+                Assembly asm = Assembly.Load("Ch15-P6-AttributedCarLibrary");
+                // Get type info of VehicleDescriptionAttribute.
+                Type vehicleDesc = asm.GetType("Ch15_P6_AttributedCarLibrary.VehicleDescriptionAttribute");
+                // Get type info of the Description property.
+                PropertyInfo propDesc = vehicleDesc.GetProperty("Description");
+                // Get all types in the assembly.
+                Type[] types = asm.GetTypes();
+                // Iterate over each type and obtain any VehicleDescriptionAttributes.
+                foreach (Type t in types)
+                {
+                    object[] objs = t.GetCustomAttributes(vehicleDesc, false);
+                    // Iterate over each VehicleDescriptionAttribute and print
+                    // the description using late binding.
+                    foreach (object o in objs)
+                    {
+                        Console.WriteLine("-> {0}: {1}\n", t.Name, propDesc.GetValue(o, null));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
