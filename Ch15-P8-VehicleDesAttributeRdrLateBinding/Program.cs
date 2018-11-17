c3199572fb62cc88c91578dda682d6f78676e4fe
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-11-17 18:46:55 +0500

    Project Created

---
+++ Ch15-P8-VehicleDesAttributeRdrLateBinding/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch15_P8_VehicleDesAttributeRdrLateBinding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
